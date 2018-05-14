9e62631f0c93646cfc8e408ea4a0b4b4553b78d7
liuhuachao <liuhuachao@foxmail.com>
2018-05-15 01:12:36 +0800

    创建.net core 2.0 web api CURD 增删改查 接口

--- src/WebApiAuth/Controllers/ProductsController.cs
+++ src/WebApiAuth/Controllers/ProductsController.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
+using WebApiAuth.Dtos;
 using WebApiAuth.Models;
 using WebApiAuth.Services;
 
@@ -11,14 +13,12 @@ namespace WebApiAuth.Controllers
     [Route("api/[controller]")]
     public class ProductsController : Controller
     {
-        // GET api/products
         [HttpGet]
         public IActionResult Get()
         {
             return Ok(ProductService.Current.Products);
         }
 
-        // GET api/products/id
         [Route("{id}", Name = "GetProduct")]
         public IActionResult GetProduct(int id)
         {
@@ -30,9 +30,8 @@ namespace WebApiAuth.Controllers
             return Ok(product);
         }
 
-        // POST api/products
         [HttpPost]
-        public IActionResult Post([FromBody] Product product)
+        public IActionResult Post([FromBody] ProductsCreation product)
         {
             if (product == null)
             {
@@ -44,6 +43,7 @@ namespace WebApiAuth.Controllers
                 ModelState.AddModelError("Name", "产品的名称不可以是'产品'二字");
             }
 
+            // 添加数据注解验证逻辑
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
@@ -60,6 +60,91 @@ namespace WebApiAuth.Controllers
             ProductService.Current.Products.Add(newProduct);
 
             return CreatedAtRoute("GetProduct", new { id = newProduct.Id }, newProduct);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] ProductModification product)
+        {
+            if (product == null)
+            {
+                return BadRequest();
+            }
+
+            if (product.Name == "产品")
+            {
+                ModelState.AddModelError("Name", "产品的名称不可以是'产品'二字");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var model = ProductService.Current.Products.SingleOrDefault(x => x.Id == id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+
+            model.Name = product.Name;
+            model.Price = product.Price;
+            model.Description = product.Description;
+
+            return NoContent();
+        }
+
+        [HttpPatch("{id}")]
+        public IActionResult Patch(int id, [FromBody] JsonPatchDocument<ProductModification> patchDoc)
+        {
+            if (patchDoc == null)
+            {
+                return BadRequest();
+            }
+            var model = ProductService.Current.Products.SingleOrDefault(x => x.Id == id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            var toPatch = new ProductModification
+            {
+                Name = model.Name,                
+                Price = model.Price,
+                Description = model.Description,
+            };
+            patchDoc.ApplyTo(toPatch, ModelState);
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (toPatch.Name == "产品")
+            {
+                ModelState.AddModelError("Name", "产品的名称不可以是'产品'二字");
+            }
+            TryValidateModel(toPatch);
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            model.Name = toPatch.Name;
+            model.Price = toPatch.Price;
+            model.Description = toPatch.Description;            
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var model = ProductService.Current.Products.SingleOrDefault(x => x.Id == id);
+            if (model == null)
+            {
+                return NotFound();
+            }
+            ProductService.Current.Products.Remove(model);
+            return NoContent();
         }
     }
 }
--- src/WebApiAuth/Startup.cs
+++ src/WebApiAuth/Startup.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -23,7 +24,13 @@ namespace WebApiAuth
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc()
+            //修改Mvc的配置来添加xml格式
+            //.AddMvcOptions(options =>
+            // {
+            //     options.OutputFormatters.Add(new XmlDataContractSerializerOutputFormatter());
+            // })
+            ;
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

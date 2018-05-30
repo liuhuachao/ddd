870161956a2ea07149dca3c92a5ada6cbccde598
liuhuachao <liuhuachao@foxmail.com>
2018-05-30 14:07:48 +0800

    修改 Swagger UI 的默认访问地址为：http://localhost:<port>/help

--- src/WebApi/Startup.cs
+++ src/WebApi/Startup.cs
@@ -140,7 +140,9 @@ namespace WebApi
             // Enable middleware to serve swagger-ui (HTML, JS, CSS etc.), specifying the Swagger JSON endpoint.
             app.UseSwaggerUI(c =>
             {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+                c.DocumentTitle = "API Document";
+                c.RoutePrefix = "help";
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
             #endregion
 

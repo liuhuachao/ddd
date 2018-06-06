8af5bf63e7ddeaff0371a6a4f2e522df85d4cd95
liuhuachao <liuhuachao@foxmail.com>
2018-06-06 14:46:33 +0800

    创建 视频控制器 VideosController

---
+++ src/WebApi/Controllers/VideosController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+    /// <summary>
+    /// 视频
+    /// </summary>
+    [Route("api/[controller]")]
+    public class VideosController : Controller
+    {
+        /// <summary>
+        /// 获取视频列表
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        /// <summary>
+        /// 根据视频 Id 获取单个视频
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+    }
+}

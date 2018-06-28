28ecb3ac7ae960cb0c0279c92cc61d8d55a25b76
liuhuachao <liuhuachao@foxmail.com>
2018-06-28 18:28:36 +0800

    Update

--- src/DDD.Common/WeixinHelper.cs
+++ src/DDD.Common/WeixinHelper.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.ComponentModel;
+using System.Linq;
 
 namespace DDD.Common
 {
@@ -30,7 +32,23 @@ namespace DDD.Common
         public static string CreateSign(string[] args)
         {
             Array.Sort(args);
-            string signature = string.Join("&", args);
+            string signature = string.Join("", args);
+            signature = Sha1(signature);
+            signature = signature.ToLower();
+            return signature;
+        }
+
+        public static string CreateJsSign(Dictionary<string,string> dic)
+        {
+            dic.OrderBy(m => m.Key);
+            Dictionary<string, string> dic2 = new Dictionary<string, string>();
+            //foreach (var item in dic)
+            //{
+            //    dic.Add(,item.Key + "=" + item.Value);
+            //}
+
+
+            string signature = string.Join("&",dic.Keys);
             signature = Sha1(signature);
             signature = signature.ToLower();
             return signature;
@@ -43,10 +61,42 @@ namespace DDD.Common
         /// <returns></returns>
         public static string Sha1(string text)
         {
-            byte[] cleanBytes = Encoding.UTF8.GetBytes(text);
+            byte[] cleanBytes = Encoding.Default.GetBytes(text);
             byte[] hashedBytes = System.Security.Cryptography.SHA1.Create().ComputeHash(cleanBytes);
             return BitConverter.ToString(hashedBytes).Replace("-", "");
         }
-
     }
+
+    /// <summary>
+    /// 微信分享js-sdk
+    /// </summary>
+    public class WeixinShare
+    {
+        private string _signature;
+
+        [DisplayName("appId")]
+        public string AppId { get; set; }
+        [DisplayName("noncestr")]
+        public string NonceStr { get; set; }
+        [DisplayName("jsapi_ticket")]
+        public string Ticket { get; set; }
+        [DisplayName("timestamp")]
+        public string Timestamp { get; set; }
+        [DisplayName("url")]
+        public string Url { get; set; }
+        [DisplayName("signature")]
+        public string Signature
+        {
+            get
+            {
+                string[] args = new string[] { this.NonceStr,this.Ticket,this.Timestamp,this.Timestamp,this.Url};
+                return WeixinHelper.CreateSign(args);
+            }
+            set
+            {
+                _signature = value;
+            }
+        }
+    }
+
 }

4ad01b0909e14954cadefe87de692a8dccfe8c30
liuhuachao <liuhuachao@foxmail.com>
2018-07-16 17:10:23 +0800

    公共方法增加Url帮助类

---
+++ src/DDD.Common/UrlHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DDD.Common
+{
+    public class UrlHelper
+    {
+        public static string Encode(string url)
+        {
+            var returnUrl = System.Web.HttpUtility.UrlEncode(url);
+            return returnUrl;
+        }
+
+        public static string Decode(string url)
+        {
+            var returnUrl = System.Web.HttpUtility.UrlDecode(url);
+            return returnUrl;
+        }
+    }
+}
